f6fa6a05aa716897caf2e1d9bbb471c3a9cf5db9
Ashwini Purohit <ashwinipurohit@yahoo.in>
2016-05-09 23:17:29 +0530

    Fixed #1
    Added file index along with file name.
    This will keep a track of the file number so that notes gets downloaded sequentially.

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
 subject_name = "";
 teacher_name = "";
 
+/* Initialize a counter to add the Notes ID before the file name.
+This will keep a track of the sequence in which notes were downloaded.
+
+*/
+var count=0; 
+
+
 chrome.runtime.onMessage.addListener
 (
 
@@ -37,7 +44,13 @@ var getLocation = function(href) {
 chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
 	if (getLocation(item.url).hostname == "academics.vit.ac.in" || getLocation(item.url).hostname == "27.251.102.132")
 	{
-		suggest({filename: "VIT Course Material"+ "/" + subject_name + "/" + teacher_name + "/" + item.filename.split("_").slice(4).join("_")});
+		/*
+		Increase the value of count to maintain sequence.
+		*/
+		count = count+1;
+		/*rename the file name along with the number.
+		*/
+		suggest({filename: "VIT Course Material"+ "/" + subject_name + "/" + teacher_name + "/" + count+"_"+ item.filename.split("_").slice(4).join("_")});
 		console.log(getLocation(item.url).hostname);
 	}
 
